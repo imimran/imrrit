3cdd169f016ea0e08492a1db2ba3e6aab30b17e0
imimran <engr.aih@gmail.com>
2021-04-29 16:32:03 +0600

    Post Vote Count

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -5,10 +5,12 @@ from .models import  Post, Vote
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.ReadOnlyField(source = 'author.username')
     author_id = serializers.ReadOnlyField(source = 'author.id')
+    votes =  serializers.SerializerMethodField()
     class Meta:
         model = Post
         fields = '__all__'
-
+    def get_votes(self, post):
+        return Vote.objects.filter(post=post).count()  
 
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
