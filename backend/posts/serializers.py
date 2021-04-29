535d15fa1924345e907c6c47dde54928c9cfa1a2
imimran <engr.aih@gmail.com>
2021-04-29 11:57:30 +0600

    Add API  permission

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -3,6 +3,8 @@ from .models import  Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source = 'author.username')
+    author_id = serializers.ReadOnlyField(source = 'author.id')
     class Meta:
         model = Post
         fields = '__all__'
