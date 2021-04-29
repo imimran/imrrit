8fafeddea16f2f96682e5ff16df830c43d34eaa6
imimran <engr.aih@gmail.com>
2021-04-29 13:30:24 +0600

    Vote Serializer & route

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -8,3 +8,10 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
+        fields = ('id')       
--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from  rest_framework import generics, permissions
-from .models import  Post
-from .serializers import PostSerializer
+from .models import  Post, Vote
+from .serializers import PostSerializer, VoteSerializer
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -10,3 +10,13 @@ class PostList(generics.ListCreateAPIView):
 
     def  perform_create(self, serializer):
         serializer.save(author =  self.request.user)
+
+class VoteCreate(generics.CreateAPIView):
+    serializer_class = VoteSerializer
+    permission_classes =  [permissions.IsAuthenticated]   
+
+    def get_queryset(self):
+        user =self.request.user
+        post = Post.objects.get(pk=kwargs['pk'])
+        return Vote.object.filter(voter = user, post=post)
+    
